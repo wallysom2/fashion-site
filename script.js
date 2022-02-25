d8f936f32031344b9368400a64738eb38731dc27
Wallyson <wallysom2@gmail.com>
2022-02-24 23:05:14 -0300

    favicon

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const FASHION_API = "https://mock-api.driven.com.br/api/v4/shirts-api/shirts";
 let contador = 0;
-let tipoModelo= null;
-let tipoGola= null;
-let tipoTecido= null;
+let tipoModelo = null;
+let tipoGola = null;
+let tipoTecido = null;
 let dados;
 let urlDigitada = null;
 
-function selecionarModelo(modeloClicado,tipoModeloEscolhido) {
+function selecionarModelo(modeloClicado, tipoModeloEscolhido) {
   const modelo = document.querySelector(".selecionado");
   tipoModelo = tipoModeloEscolhido;
   if (modelo !== null) {
@@ -44,35 +44,35 @@ function selecionarTecido(tecidoClicado, tipoTecidoEscolhido) {
 
 function liberarBotao() {
   let liberar = "não"
-  const linkPreencido = document.querySelector (".receberLink")
+  const linkPreencido = document.querySelector(".receberLink")
   urlDigitada = linkPreencido.value;
 
-  if (linkPreencido.value.match(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gmi) !== null){
-      liberar = "sim"
-      console.log (liberar)
+  if (linkPreencido.value.match(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gmi) !== null) {
+    liberar = "sim"
+    console.log(liberar)
   } else {
-   const naoPodeClicar = document.querySelector (".botaoClicavel")
-   naoPodeClicar.innerHTML = `<button class="botao">Confirmar pedido</button>`
-   const linkInvalido = document.querySelector (".receberLink")
-   linkInvalido.value ="";
+    const naoPodeClicar = document.querySelector(".botaoClicavel")
+    naoPodeClicar.innerHTML = `<button class="botao">Confirmar pedido</button>`
+    const linkInvalido = document.querySelector(".receberLink")
+    linkInvalido.value = "";
   }
 
-if (contador >=3 && liberar=="sim") {
-  const botaoPodeClicar = document.querySelector (".botaoClicavel")
-    botaoPodeClicar.innerHTML = `<button class="botao" onclick="postBlusaFeita()"> Confirmar pedido</button>`  
-  const botaoAzul = document.querySelector(".botao")
-    botaoAzul.classList.add("botaoLiberado") 
-  } 
+  if (contador >= 3 && liberar == "sim") {
+    const botaoPodeClicar = document.querySelector(".botaoClicavel")
+    botaoPodeClicar.innerHTML = `<button class="botao" onclick="postBlusaFeita()"> Confirmar pedido</button>`
+    const botaoAzul = document.querySelector(".botao")
+    botaoAzul.classList.add("botaoLiberado")
+  }
 }
 
-function obterBlusas () {
-  const promise = axios.get (`${FASHION_API}`)
-  promise.then (resposta => {
-    console.log (resposta.data)
-    renderizarBlusas (resposta.data)
+function obterBlusas() {
+  const promise = axios.get(`${FASHION_API}`)
+  promise.then(resposta => {
+    console.log(resposta.data)
+    renderizarBlusas(resposta.data)
   });
-  promise.catch (erro => {
-    console.error (erro.response);
+  promise.catch(erro => {
+    console.error(erro.response);
     alert("Xiii! Deu ruim na hora de receber mensagens!");
   })
 }
@@ -81,47 +81,42 @@ function renderizarBlusas(blusas) {
   dataBlusas = blusas
   const mostrarBlusas = document.querySelector(".ultimosPedidos");
   mostrarBlusas.innerHTML = "";
-  blusas.forEach( blusa => {
-      const id = blusa.id;
-      const autor = blusa.owner;
-      const imagem = blusa.image;
-  
- 
-      mostrarBlusas.innerHTML +=  `
+  blusas.forEach(blusa => {
+    const id = blusa.id;
+    const autor = blusa.owner;
+    const imagem = blusa.image;
+
+
+    mostrarBlusas.innerHTML += `
       <div class="ultimosPedidosCada">
       <img src="${imagem}" alt="Blusa">
       <div><span>Criador: </span>${autor}</div>
       </div>
         `
-  }) }
+  })
+}
 
-  function postBlusaFeita (){
-    const promise = axios.post(
-      `${FASHION_API}`,
-      {
-        "model": tipoModelo,
-        "neck": tipoGola,
-        "material": tipoTecido,
-        "image": urlDigitada,
-        "owner": nome,
-        "author": nome
-      });
-      promise.then (resposta => {
-       // alert("Encomenda confirmada  =)"); 
-        console.log (resposta.data)
-        obterBlusas ();
-        console.log (resposta.data)
-      });
-      promise.catch (erro => {
-        console.error (erro.response);
-        alert("Ops, não conseguimos processar sua encomenda"); 
-      })
-  }
+function postBlusaFeita() {
+  const promise = axios.post(
+    `${FASHION_API}`, {
+      "model": tipoModelo,
+      "neck": tipoGola,
+      "material": tipoTecido,
+      "image": urlDigitada,
+      "owner": nome,
+      "author": nome
+    });
+  promise.then(resposta => {
+    // alert("Encomenda confirmada  =)"); 
+    console.log(resposta.data)
+    obterBlusas();
+    console.log(resposta.data)
+  });
+  promise.catch(erro => {
+    console.error(erro.response);
+    alert("Ops, não conseguimos processar sua encomenda");
+  })
+}
 
- let nome = prompt ("Qual o seu nome?  ")
-  obterBlusas ();
- 
-  
-
-
- 
+let nome = prompt("Qual o seu nome?  ")
+obterBlusas();
